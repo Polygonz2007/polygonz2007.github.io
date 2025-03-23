21db1dc64de9f61598bd645e4f20b43a9356f269
Polygonz <156954786+Polygonz2007@users.noreply.github.com>
2025-03-23 14:22:51 +0100

    Location

--- BackgroundTimer/time.js
+++ BackgroundTimer/time.js
@@ -13,3 +13,24 @@
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 })()
+
+const x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function success(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function error() {
+  alert("Sorry, no position available.");
+}
+
+getLocation();
